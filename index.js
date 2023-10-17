4289c6dad38177826f9cb5941b38b8011ac6773d
Melanie Elizabeth Damm <92762328+HarleyVader@users.noreply.github.com>
2023-10-17 14:10:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const { clientId, clientSecret, port } = require('./config.json');
 
 const app = express();
-
-app.get('/', async ({ query }, response) => {
+app.get('/connect', async ({ query }, response) => {
     const { code } = query;
 
     if (code) {
         try {
             const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+
                 method: 'POST',
                 body: new URLSearchParams({
                     client_id: clientId,
@@ -25,12 +25,30 @@ app.get('/', async ({ query }, response) => {
             });
 
             const oauthData = await tokenResponseData.body.json();
+            
+            if (oauthData.error) {
+            return response.send({ status: "Not Ok" });
+            }
+            console.log("OAuth Check : " + code);
             console.log(oauthData);
+
+            let discordUser = await fetch("https://discord.com/api/users/@me", {
+            headers: {
+                authorization: `${oauthData.token_type} ${oauthData.access_token}`
+            }
+            });
+            discordUser = await discordUser.json();
+            console.log(discordUser);
+            console.log(oauthData);
+            return response.send(discordUser);
         } catch (error) {
             console.error(error);
         }
     }
-
+    return response.send({ status: "Not Ok" });
+    
+});
+app.get('/', (_request, response) => {
     // Send the HTML file as a response
     return response.sendFile('index.html', { root: __dirname });
 });
